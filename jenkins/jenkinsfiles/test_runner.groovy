b5918b1bfa2e54468b9e63904ab5da19f4ea73c2
Ahmad Wahbi <139769584+AhmadSealights@users.noreply.github.com>
2026-02-04 10:30:19 +0200

    Refactor test_runner.groovy to remove slnodejs commands
    Removed slnodejs start and end commands from test runner.

--- jenkins/jenkinsfiles/test_runner.groovy
+++ jenkins/jenkinsfiles/test_runner.groovy
@@ -122,9 +122,7 @@ pipeline {
                       "labid": "${params.SL_LABID}",
                       "testStage": "Cucumber Tests"
                       }' > sl.conf
-                    ./node_modules/.bin/slnodejs start --tokenfile ./sltoken.txt --labid ${params.SL_LABID} --teststage "Cucumber Tests"
                     node_modules/.bin/cucumber-js ./features --require \$SL_PACKAGE --require 'features/**/*.@(js|cjs|mjs)'
-                    ./node_modules/.bin/slnodejs end --tokenfile ./sltoken.txt --labid ${params.SL_LABID}
                     sleep ${env.wait_time}
                     """
             }
